73d0c34293f1e23c92cded8357472717e508c76c
Patrick Porto <patrickporto.dev@gmail.com>
2021-06-24 17:34:17 -0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-table hover striped :items="items"></b-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default{
+  data(){
+    return{
+      items:[{
+        name: 'Joao',
+        idade: 20
+      },
+      {
+        name: 'Maria',
+        idade: 20
+      },
+      {
+        name: 'Joao',
+        idade: 20
+      }]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
